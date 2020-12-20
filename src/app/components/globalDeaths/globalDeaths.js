d655c8f5edf123b9225ea57c22817bdb62d241a2
Dmitry Melnikov <dmitry.melni@gmail.com>
2020-12-20 11:54:58 +0300

    fix: table filtering logic

--- src/app/components/globalDeaths/globalDeaths.js
+++ src/app/components/globalDeaths/globalDeaths.js
@@ -8,38 +8,99 @@ export default class GlobalDeaths {
     const dataBase = new DataBase();
     const loadingAnimate = new LoadingAnimation();
     const select = document.querySelector("#globalDeaths_select");
-    const module = document.querySelector(".globalDeaths");
+    const table = document.querySelector(".globalDeaths_table");
+    let activeCountry = "all";
+    let fullTableType = "all";
     dataBase.init();
     document
       .querySelector(".globalDeaths")
       .append(loadingAnimate.init().loadAnimateInner);
     loadingAnimate.startAnimate();
-    this.summary = await dataBase.getSummaryFromApi();
-    GlobalDeaths.generateTableGlobalDeath(this.summary);
+    const summary = await dataBase.getSummaryFromApi();
+    GlobalDeaths.generateTableGlobalDeath(
+      summary,
+      "Global Deaths",
+      "all",
+      fullTableType
+    );
     loadingAnimate.stopAnimate();
     select.addEventListener("change", () => {
       if (select.value === "Global Deaths") {
-        GlobalDeaths.generateTableGlobalDeath(this.summary, "Global Deaths");
+        if (document.querySelector(".globalDeaths_row--active") !== null) {
+          fullTableType = "Global Recovered";
+        } else if (
+          document.querySelectorAll(".globalDeaths_row").length === 1
+        ) {
+          fullTableType = "all";
+        } else {
+          activeCountry = "all";
+          fullTableType = "all";
+        }
+        GlobalDeaths.generateTableGlobalDeath(
+          summary,
+          "Global Deaths",
+          activeCountry,
+          fullTableType
+        );
       } else {
-        GlobalDeaths.generateTableGlobalDeath(this.summary, "Global Recovered");
+        if (document.querySelector(".globalDeaths_row--active") !== null) {
+          fullTableType = "Global Deaths";
+        } else if (
+          document.querySelectorAll(".globalDeaths_row").length === 1
+        ) {
+          fullTableType = "all";
+        } else {
+          activeCountry = "all";
+          fullTableType = "all";
+        }
+        GlobalDeaths.generateTableGlobalDeath(
+          summary,
+          "Global Recovered",
+          activeCountry,
+          fullTableType
+        );
       }
     });
-    module.addEventListener("click", (event) => {
+    table.addEventListener("click", (event) => {
       const targetRow = event.target.closest("tr");
       const currentActive = document.querySelector(".globalDeaths_row--active");
+      let targetCountryCount = targetRow.querySelector(
+        ".globalDeaths_cell--count"
+      ).textContent;
+      targetCountryCount = parseInt(
+        targetCountryCount.replace(/[^\d]/g, ""),
+        10
+      );
       if (currentActive !== null) {
-        currentActive.classList.remove("globalDeaths_row--active");
-      }
-      if (targetRow.classList.contains("globalDeaths_row--active")) {
-        event.target.closest("tr").classList.remove("globalDeaths_row--active");
+        if (targetRow.classList.contains("globalDeaths_row--active")) {
+          currentActive.classList.remove("globalDeaths_row--active");
+          GlobalDeaths.updateTotalCount(select, summary);
+        } else {
+          currentActive.classList.remove("globalDeaths_row--active");
+          targetRow.classList.add("globalDeaths_row--active");
+          activeCountry = targetRow.querySelector(".globalDeaths_cell--country")
+            .textContent;
+          GlobalDeaths.setCount(targetCountryCount);
+        }
       } else {
-        event.target.closest("tr").classList.add("globalDeaths_row--active");
+        targetRow.classList.add("globalDeaths_row--active");
+        activeCountry = targetRow.querySelector(".globalDeaths_cell--country")
+          .textContent;
+        GlobalDeaths.setCount(targetCountryCount);
       }
     });
     return this;
   }
 
-  static generateTableGlobalDeath(summary, type) {
+  static updateTotalCount(select, summary) {
+    if (select.value === "Global Deaths") {
+      GlobalDeaths.setCount(summary.Global.TotalDeaths);
+    } else {
+      GlobalDeaths.setCount(summary.Global.TotalRecovered);
+    }
+  }
+
+  static generateTableGlobalDeath(summary, type, targetCountry, optionsTable) {
     const title = document.querySelector(".globalDeaths_title");
     const count = document.querySelector(".globalDeaths_count");
     const table = document.querySelector(".globalDeaths_table");
@@ -63,33 +124,51 @@ export default class GlobalDeaths {
         break;
     }
     arrData.forEach((country) => {
-      table.append(GlobalDeaths.createRow(country, type));
+      table.append(
+        GlobalDeaths.createRow(country, type, targetCountry, optionsTable)
+      );
     });
   }
 
-  static createRow(country, type) {
-    const newRow = CreateDomElement("tr", "globalDeaths_row");
+  static createRow(countrys, type, targetCountry, optionsTable) {
+    let newRow = CreateDomElement("tr", "globalDeaths_row");
     const cellCount = CreateDomElement("td", "globalDeaths_cell");
     const cellCountry = CreateDomElement("td", "globalDeaths_cell");
+    cellCount.classList.add("globalDeaths_cell--count");
+    cellCountry.classList.add("globalDeaths_cell--country");
     let countNumb = "";
     if (type === "Global Recovered") {
-      countNumb = country.TotalRecovered.toString().replace(
+      countNumb = countrys.TotalRecovered.toString().replace(
         /(\d)(?=(\d\d\d)+([^\d]|$))/g,
         "$1 "
       );
       cellCount.append(`${countNumb} recovered`);
       cellCount.classList.add("globalDeaths_cell--recovered");
     } else {
-      countNumb = country.TotalDeaths.toString().replace(
+      countNumb = countrys.TotalDeaths.toString().replace(
         /(\d)(?=(\d\d\d)+([^\d]|$))/g,
         "$1 "
       );
       cellCount.append(`${countNumb} deaths`);
       cellCount.classList.remove("globalDeaths_cell--recovered");
     }
-    cellCountry.append(country.Country);
-    newRow.append(cellCount);
-    newRow.append(cellCountry);
+    if (
+      targetCountry === "all" ||
+      targetCountry === countrys.Country ||
+      optionsTable === "all"
+    ) {
+      if (targetCountry === countrys.Country && optionsTable === "all") {
+        newRow.classList.add("globalDeaths_row--active");
+      }
+      if (targetCountry === countrys.Country) {
+        GlobalDeaths.setCount(countNumb);
+      }
+      cellCountry.append(countrys.Country);
+      newRow.append(cellCount);
+      newRow.append(cellCountry);
+    } else {
+      newRow = "";
+    }
     return newRow;
   }
 
